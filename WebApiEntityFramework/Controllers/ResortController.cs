78778f680a5e8165d30ad894776c89f700bdad11
brav88 <brav88@hotmail.com>
2025-06-05 20:11:36 -0600

    Agregar archivos de proyecto.

---
+++ WebApiEntityFramework/Controllers/ResortController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiEntityFramework.DatabaseHelper;
+using WebApiEntityFramework.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiEntityFramework.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ResortController : ControllerBase
+	{
+		private readonly AppDbContext _context;
+
+		public ResortController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		// GET: api/<ResortController>
+		[HttpGet]
+		public async Task<List<Resort>> Get()
+		{
+			return await _context.Resorts.ToListAsync();
+		}
+
+		// GET api/<ResortController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<ResortController>
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/<ResortController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<ResortController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ WebApiEntityFramework/DatabaseHelper/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiEntityFramework.Model;
+
+namespace WebApiEntityFramework.DatabaseHelper
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options)
+			: base(options) { }
+
+		public DbSet<Resort> Resorts { get; set; }
+	}
+}
